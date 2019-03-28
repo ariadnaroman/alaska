5765a64ee9322f034bf7d0cccd1bb9b936cb9b03
ariadnaroman <roman.ariadna@gmail.com>
2019-03-28 17:50:15 +0200

    Generate pages for login, register and dashboard; Template authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', loadChildren: './public/login/login.module#LoginPageModule'},
+    {path: 'register', loadChildren: './public/register/register.module#RegisterPageModule'},
+    {
+        path: 'private',
+        canActivate: [AuthGuard],
+        loadChildren: './private/private-routing.module#PrivateRoutingModule'
+    }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
