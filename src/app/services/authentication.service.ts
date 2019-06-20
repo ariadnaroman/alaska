38eff131161b200db8d69dcc1d83ea33cdeda74b
ariadnaroman <roman.ariadna@gmail.com>
2019-06-20 18:20:16 +0300

    Fix registration

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs/index";
 import {Storage} from "@ionic/storage";
 import {Platform} from "@ionic/angular";
-const crypto = require('crypto');
 const axios = require('axios');
 
 
@@ -44,13 +43,11 @@ export class AuthenticationService {
     }
 
     async register(username: string, password: string) {
-        const salt = AuthenticationService.generateSalt();
-        const encryptedPassword = AuthenticationService.sha512EncryptPassword(password, salt);
         const response = await axios.post("http://localhost:3000/api/auth/signup", {
-            username, password: encryptedPassword, salt
+            username, password
         });
         await this.storage.set(USER, response.data.user);
-        return this.storage.set(TOKEN_KEY, `Bearer ${response.token}`).then(() => {
+        return this.storage.set(TOKEN_KEY, `Bearer ${response.data.token}`).then(() => {
             this.authenticationState.next(true);
         });
     }
@@ -65,25 +62,4 @@ export class AuthenticationService {
         return this.authenticationState.value;
     }
 
-    encrypt(password) {
-        const algorithm = 'aes-256-cbc';
-        const key = crypto.randomBytes(32);
-        const iv = crypto.randomBytes(16);
-
-        let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-        let encrypted = cipher.update(password);
-        encrypted = Buffer.concat([encrypted, cipher.final()]);
-        return { iv: iv.toString('hex'), key: key.toString('hex'), password: encrypted.toString('hex') };
-    }
-
-
-    static generateSalt() {
-        return crypto.randomBytes(8).toString('hex').slice(0, 16);
-    }
-
-    static sha512EncryptPassword(password: string, salt: string) {
-        const hash = crypto.createHmac('sha512', salt);
-        hash.update(password);
-        return hash.digest('hex');
-    }
 }
